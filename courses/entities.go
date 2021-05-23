c1a99a546be1112adce7f1a85c4c5a06497f0988
waldemar <hellodoge@tuta.io>
2021-05-23 20:05:54 +0300

    Updated Photo, Video and Document entities (JSON modifications and add Document.Description)

--- courses/entities.go
+++ courses/entities.go
@@ -1,15 +1,16 @@
 package courses
 
 type Video struct {
-	Description string `json:"description,omitempty" bson:"description,omitempty"`
-	URLCollection
+	Description string        `json:"description,omitempty" bson:"description,omitempty"`
+	URLs        URLCollection `json:"url_collection" bson:"url_collection"`
 }
 
 type Photo struct {
-	Description string `json:"description,omitempty" bson:"description,omitempty"`
-	URLCollection
+	Description string        `json:"description,omitempty" bson:"description,omitempty"`
+	URLs        URLCollection `json:"url_collection" bson:"url_collection"`
 }
 
 type Document struct {
-	URLCollection
+	Description string        `json:"description,omitempty" bson:"description,omitempty"`
+	URLs        URLCollection `json:"url_collection" bson:"url_collection"`
 }
