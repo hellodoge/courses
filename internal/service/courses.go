127af78ea25ca235da06de257646923576e12eb5
waldemar <hellodoge@tuta.io>
2021-05-23 15:07:19 +0300

    Add 'courses' service

---
+++ internal/service/courses.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"github.com/hellodoge/courses-tg-bot/courses"
+	"github.com/hellodoge/courses-tg-bot/internal/repository"
+)
+
+type CoursesService struct {
+	repo repository.Courses
+}
+
+func NewCoursesService(repo repository.Courses) *CoursesService {
+	return &CoursesService{repo: repo}
+}
+
+func (s *CoursesService) NewCourse(course *courses.Course) (string, error) {
+	return s.repo.NewCourse(course)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,20 +1,29 @@
 package service
 
-import "github.com/hellodoge/courses-tg-bot/internal/repository"
+import (
+	"github.com/hellodoge/courses-tg-bot/courses"
+	"github.com/hellodoge/courses-tg-bot/internal/repository"
+)
 
 type Roles interface {
 	NewAdmin(description string) (string, error)
 	NewModerator(description string) (string, error)
 }
 
+type Courses interface {
+	NewCourse(course *courses.Course) (string, error)
+}
+
 type Service struct {
 	Telegram *TelegramService
+	Courses
 	Roles
 }
 
 func NewService(repository *repository.Repository) *Service {
 	return &Service{
 		Telegram: NewTelegramService(repository.Telegram),
+		Courses:  NewCoursesService(repository.Courses),
 		Roles:    NewRolesService(repository.Roles),
 	}
 }
